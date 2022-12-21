6be0dbe2f1104183b4ca91250c9e0097d38997c3
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-12-21 09:05:46 +0530

    Successfully added the new post and now moving forward to add the permissions in the django backend for making this backend

--- blog_api/views.py
+++ blog_api/views.py
@@ -4,12 +4,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from blog.models import Post
 from .serializer import PostSerializer
-# Create your views here.
+
+
+
 # here we will be creating our views in order to give the response to the apis
 # we are building backend in django 
 class PostDetails(APIView):
     # in this we can define the get and the post request using the function as done below for this purpose 
     def get(self, request, pk):
+
         # the arguments of the url can be access using the kwargs if the arguments are named in the url 
         # hence we can access the primary key using the following code 
         primaryKey = self.kwargs['pk'];
@@ -19,30 +22,14 @@ class PostDetails(APIView):
 
 
         # we have to fetch this data from the database and put it in the serialzer for this purpose 
-        currentPost = Post.objects.get(pk = pk);
-        print("The value of the current post fetched from the database with the current id is as follows \n", currentPost);
-        currentPost = PostSerializer(currentPost);
-        # print("T")
+        currentPost = Post.objects.filter(id = primaryKey);
+        currentPost = PostSerializer(currentPost, many=True);
+        print("The post after serialization is ", currentPost.data);
+
         # say everything went fine 
         return Response(currentPost.data);
 
-    # now we also have to define the post request for creating new post 
-    def post(self, request):
-        print("The new post that i am going to insert into the database is as follows \n", request.data);
-        serializedData = PostSerializer(data=request.data);
-        print("The actual post after serialization is as follows \n", serializedData);
 
-        # now we have to save it 
-        if serializedData.is_valid() :
-            # then we can save this into the database 
-            serializedData.save();
-        else:
-            # this is not valid hence we can raise a error to the client side 
-            return Response(serializedData.errors, status=status.HTTP_400_BAD_REQUEST);
-        
-        # this means we have successfully saved the new entry in the database
-        # say everything went fine  
-        return Response(serializedData.data, status=status.HTTP_201_CREATED);
 
 
     
@@ -60,4 +47,20 @@ class PostList(APIView):
         
         # say everything went fine 
         return Response(serializedData.data);
-    
+        
+     # now we also have to define the post request for creating new post 
+    def post(self, request):
+        print("The new post that i am going to insert into the database is as follows \n", request.data);
+        serializedData = PostSerializer(data=request.data);
+        print("The actual post after serialization is as follows \n", serializedData);
+
+        # now we have to save it 
+        if serializedData.is_valid() :
+            # then we can save this into the database 
+            serializedData.save();
+        else:
+            # this is not valid hence we can raise a error to the client side 
+            return Response(serializedData.errors, status=status.HTTP_400_BAD_REQUEST);
+        
+        # this means we have successfully saved the new entry in the database
+        return Response(serializedData.data, status=status.HTTP_201_CREATED);
