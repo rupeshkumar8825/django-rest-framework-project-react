c07eb2191412a160957d3ae05a0c24c8655bb02e
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-12-27 13:10:32 +0530

    Successfully added the cors and made a get api call to the django backend

--- DRF-REACT/blogapi/src/App.js
+++ DRF-REACT/blogapi/src/App.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+
+
+
+
 
 const App = ()=>{
+
+  // defining the function to store the list of the post 
+  const getAllPost = async ()=>{
+
+
+	console.log("calling the getall post method inside the use effect ");
+	// we have to use the axios to make a post request 
+	const URL = "http://127.0.0.1:8000/api/";
+	const response = await  axios.get(URL);
+	console.log("The reponse from the backend is as follows \n");
+	console.log(response.data);
+  }
+  // let us use a useffec 
+  useEffect(() => {
+    getAllPost()
+    return () => {
+      
+    };
+  }, []);
   return (
     <>
       <h1>this is this main content of the application </h1>
