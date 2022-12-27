e72c1ab05097fee9b67ab16596de2a210ef0f2d5
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-12-27 11:35:42 +0530

    Added the header and footer now we will be dealing with api from the django backend to make a full fledged project using django backend and reactjs frontend for learning purpose

--- DRF-REACT/blogapi/src/components/Footer.js
+++ DRF-REACT/blogapi/src/components/Footer.js
@@ -1,13 +1,106 @@
-import React from 'react'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
 
-const Footer = ()=>{
-    return (
-        <>
-            <h1> hi this is footer of the application</h1>
-        </>
-    )
+const useStyles = makeStyles((theme) => ({
+	footer: {
+		borderTop: `1px solid ${theme.palette.divider}`,
+		marginTop: theme.spacing(8),
+		paddingTop: theme.spacing(3),
+		paddingBottom: theme.spacing(3),
+		[theme.breakpoints.up('sm')]: {
+			paddingTop: theme.spacing(6),
+			paddingBottom: theme.spacing(6),
+		},
+	},
+}));
+
+
+// this is the copyright section of the footer in the form of function which will help 
+// us looking the different section of the same component in clean and appropriate manner 
+function Copyright() {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 
-// say everything went fine 
+// this is array of the content that we need to store in the footers 
+const footers = [
+	{
+		title: 'Company',
+		description: ['Team', 'History', 'Contact us', 'Locations'],
+	},
+	{
+		title: 'Features',
+		description: [
+			'Cool stuff',
+			'Random feature',
+			'Team feature',
+			'Developer stuff',
+			'Another one',
+		],
+	},
+	{
+		title: 'Resources',
+		description: [
+			'Resource',
+			'Resource name',
+			'Another resource',
+			'Final resource',
+		],
+	},
+	{
+		title: 'Legal',
+		description: ['Privacy policy', 'Terms of use'],
+	},
+];
+
+
+// this is the actual footer component function that we will be exporting for this purpose 
+function Footer() {
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<Container maxWidth="md" component="footer" className={classes.footer}>
+				<Grid container spacing={4} justify="space-evenly">
+                    {/* here we are using the map function in reactjs to map each element of the footer  */}
+					{footers.map((footer) => (
+						<Grid item xs={6} sm={3} key={footer.title}>
+                            {/* here we are adding the footer.title to show the title of the footer  */}
+							<Typography variant="h6" color="textPrimary" gutterBottom>
+								{footer.title}
+							</Typography>
+							<ul>
+                                {/* then since we have multiple descriptions hence we again are using map  */}
+								{footer.description.map((item) => (
+									<li key={item}>
+										<Link href="#" variant="subtitle1" color="textSecondary">
+											{item}
+										</Link>
+									</li>
+								))}
+							</ul>
+						</Grid>
+					))}
+				</Grid>
+				<Box mt={5}>
+					<Copyright />
+				</Box>
+			</Container>
+		</React.Fragment>
+	);
+}
+
 export default Footer;
--- DRF-REACT/blogapi/src/components/Header.js
+++ DRF-REACT/blogapi/src/components/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import {Toolbar} from '@material-ui/core';
+import {Typography} from '@material-ui/core';
+import {CssBaseline} from '@material-ui/core';
+import { makeStyles }  from '@material-ui/core';
+
+
+// making some changes to the css properties apart from the material ui 
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderBottom : `1px solid ${theme.palette.divider}`,
+    }
+}));
 
 
 // now we have to make the react component 
 const Header = () =>{
+    const classes = useStyles();
+
     return (
-        <>
-            <h1>this is the header of the application</h1>
-        </>
+        <React.Fragment>
+            <CssBaseline></CssBaseline>
+            <AppBar position = "static" color="white" elevation = {0} className = {classes.appBar}>
+                <Toolbar>
+                    <Typography variate = "h6" color = "inherit" noWrap>
+                        BlogmeUp
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            {/* <h1>this is the header of the application</h1> */}
+        </React.Fragment>
     )
 }
 
